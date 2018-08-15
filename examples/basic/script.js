1aad215132eafc0aee810358828afa9c5c22285c
Jan Umansky <dokydok@gmail.com>
2018-08-15 13:03:40 -0400

    blizzard content

--- examples/basic/script.js
+++ examples/basic/script.js
@@ -7,10 +7,10 @@ var url = 'https://www.videoflow.io/experience/HJBGPfl77';
 var conf = {
     key: '42679f1a-302b-40c7-a459-927957b07a4e',
     source: {
-        dash: 'https://bitmovin-a.akamaihd.net/content/MI201109210084_1/mpds/f08e80da-bf1d-4e3d-8899-f0f6155f6efa.mpd',
-        hls: 'https://bitmovin-a.akamaihd.net/content/MI201109210084_1/m3u8s/f08e80da-bf1d-4e3d-8899-f0f6155f6efa.m3u8',
-        progressive: 'https://bitmovin-a.akamaihd.net/content/MI201109210084_1/MI201109210084_mpeg-4_hd_high_1080p25_10mbits.mp4',
-        poster: 'https://bitmovin-a.akamaihd.net/content/MI201109210084_1/poster.jpg'
+        //dash: 'https://bitmovin-a.akamaihd.net/content/MI201109210084_1/mpds/f08e80da-bf1d-4e3d-8899-f0f6155f6efa.mpd',
+        hls: 'https://d32qh16dbjppfl.cloudfront.net/5928aba013a23e2d84e17a19/HyY2IishG/video2/720/index.m3u8',
+        //progressive: 'https://bitmovin-a.akamaihd.net/content/MI201109210084_1/MI201109210084_mpeg-4_hd_high_1080p25_10mbits.mp4',
+        //poster: 'https://bitmovin-a.akamaihd.net/content/MI201109210084_1/poster.jpg'
     }
 };
 var player = bitmovin.player('player');
